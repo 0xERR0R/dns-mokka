335e64fe7e448c24d80b16e5f71af22a2fb58bee
Dimitri Herzog <dimitri.herzog@gmail.com>
2025-10-28 22:08:57 +0100

    test: add additional tests and docs

--- mock/functions_test.go
+++ mock/functions_test.go
@@ -68,6 +68,44 @@ var _ = Describe("Functions", func() {
 				Expect(result.Err.Error()).Should(ContainSubstring("record should be in format"))
 			})
 
+			It("should return valid response with various record types", func() {
+				execute, err := vm.Execute(e, nil, `NOERROR("MX 10 mail.example.com. 300", "TXT \"hello world\" 300", "CNAME www.example.com. 300", "NS ns1.example.com. 300", "PTR www.example.com. 300", "SRV 10 60 5060 sip.example.com. 300", "CAA 0 issue \"letsencrypt.org\" 300")`)
+				Expect(err).Should(Succeed())
+				result := execute.(mock.Result)
+
+				Expect(result.Err).Should(BeNil())
+				Expect(result.RCode).Should(Equal(dns.RcodeSuccess))
+				Expect(result.RR).Should(HaveLen(7))
+
+				Expect(result.RR[0].RType).Should(Equal("MX"))
+				Expect(result.RR[0].Address).Should(Equal("10 mail.example.com."))
+				Expect(result.RR[0].TTL).Should(Equal(300))
+
+				Expect(result.RR[1].RType).Should(Equal("TXT"))
+				Expect(result.RR[1].Address).Should(Equal(`"hello world"`))
+				Expect(result.RR[1].TTL).Should(Equal(300))
+
+				Expect(result.RR[2].RType).Should(Equal("CNAME"))
+				Expect(result.RR[2].Address).Should(Equal("www.example.com."))
+				Expect(result.RR[2].TTL).Should(Equal(300))
+
+				Expect(result.RR[3].RType).Should(Equal("NS"))
+				Expect(result.RR[3].Address).Should(Equal("ns1.example.com."))
+				Expect(result.RR[3].TTL).Should(Equal(300))
+
+				Expect(result.RR[4].RType).Should(Equal("PTR"))
+				Expect(result.RR[4].Address).Should(Equal("www.example.com."))
+				Expect(result.RR[4].TTL).Should(Equal(300))
+
+				Expect(result.RR[5].RType).Should(Equal("SRV"))
+				Expect(result.RR[5].Address).Should(Equal("10 60 5060 sip.example.com."))
+				Expect(result.RR[5].TTL).Should(Equal(300))
+
+				Expect(result.RR[6].RType).Should(Equal("CAA"))
+				Expect(result.RR[6].Address).Should(Equal(`0 issue "letsencrypt.org"`))
+				Expect(result.RR[6].TTL).Should(Equal(300))
+			})
+
 			It("should return valid response with RRSIG record", func() {
 				execute, err := vm.Execute(e, nil,
 					`NOERROR("A 1.2.3.4 300", "RRSIG A 13 3 300 20991231235959 20230101000000 12345 invalid.dnssec.test. fakesignaturedata== 300")`)
